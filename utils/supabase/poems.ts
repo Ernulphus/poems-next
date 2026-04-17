54bddfd63b8a282d01da60f01e7ca672a8774ac8
Boaz Kaufman <bk1nyc@gmail.com>
2026-04-16 22:19:30 -0400

    Add fn to create a poem

--- utils/supabase/poems.ts
+++ utils/supabase/poems.ts
@@ -1,6 +1,19 @@
 import { createClient } from '@/utils/supabase/client';
 
+type Poem = {
+	title: string;
+	text: string;
+	publish_year: number;
+	author: number;
+	tags: string;
+};
+
 export async function getPoem(id: number) {
 	const supabase = createClient();
 	return supabase.from('poems').select().eq('id', id).single();
+}
+
+export async function createPoem(rowDict: Poem) {
+	const supabase = createClient();
+	return supabase.from('poems').insert(rowDict);
 }
